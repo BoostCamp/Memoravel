0232361c07184cb10cf38750670d5ffb231f28ec
JUNYEONG.YOO <woody.chiz@gmail.com>
2017-02-22 17:56:10 +0900

    Add animation both map and collection view simultaneously

--- Memoravel/PlayJourneyViewController.swift
+++ Memoravel/PlayJourneyViewController.swift
@@ -8,15 +8,31 @@
 
 import UIKit
 import MapKit
+import Photos
 
 class PlayJourneyViewController: UIViewController {
 	
-	var schedules: [Schedule]!
+//	var schedules: [Schedule]!
+	var journey: Journey!
+	
 	var timer: Timer?
-	var loIndex: Int = 0
+	let widthOfCell: CGFloat = 150.0
+	
+	var scheduleAssets = [[TravelAsset]]()
+	var allAssets = [TravelAsset]()
 
 	@IBOutlet weak var mapView: MKMapView!
 	@IBOutlet weak var collectionView: UICollectionView!
+	
+	// Properties for animating images
+	var progress: CGFloat = 0.0
+	var contentWidth: CGFloat = 0.0
+	var locationIndex: Int = 0
+	var scheduleOffset: [CGFloat] = [0.0]
+	
+	// Properties for Map view
+	var polyline: MKGeodesicPolyline?
+	var annotations: [MKPointAnnotation]?
 	
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,51 +41,32 @@ class PlayJourneyViewController: UIViewController {
 		self.collectionView.delegate = self
 		self.collectionView.dataSource = self
 		
-		// Set initial map status
-		let annotation = MKPointAnnotation()
-		let location: MKPlacemark = self.schedules[self.loIndex].location
+		// Extract schedule assets from schedules array
+		self.appendScheduleAssets()
+//		self.animateLocationsOnMap(index: 0)
 		
-		annotation.coordinate = location.coordinate
-		annotation.title = location.name
-		
-		if let city = location.locality, let state = location.administrativeArea, let country = location.country {
-			annotation.subtitle = "\(city) \(state), \(country)"
-		}
-		
-		print("LOCATION: \(location.name!)")
-		
-		self.mapView.addAnnotation(annotation)
-		self.mapView.selectAnnotation(annotation, animated: true)
-		
-		let span = MKCoordinateSpanMake(0.05, 0.05)
-		let region = MKCoordinateRegionMake(location.coordinate, span)
-		self.mapView.setRegion(region, animated: true)
-
-        // Set Timer
-		self.timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(self.animateLocationsOnMap), userInfo: nil, repeats: true)
+		// Set the delegate of the MKMap
+		self.mapView.delegate = self
     }
 	
-	public func animateLocationsOnMap() {
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		configAutoScrollTimer()
+	}
+	
+	override func viewDidDisappear(_ animated: Bool) {
+		super.viewDidDisappear(animated)
+		deconfigAutoScrollTimer()
 		
-		self.loIndex = (self.loIndex + 1 < self.schedules.count) ? self.loIndex + 1 : 0
-		let location: MKPlacemark = self.schedules[self.loIndex].location
-		
-		let annotation = MKPointAnnotation()
-		annotation.coordinate = location.coordinate
-		annotation.title = location.name
-		
-		if let city = location.locality, let state = location.administrativeArea, let country = location.country {
-			annotation.subtitle = "\(city) \(state), \(country)"
+		// Delete all overlays of Map view
+		for overlay in self.mapView.overlays {
+			self.mapView.remove(overlay)
 		}
 		
-		print("LOCATION: \(location.name!)")
-		
-		self.mapView.addAnnotation(annotation)
-		self.mapView.selectAnnotation(annotation, animated: true)
-		
-		let span = MKCoordinateSpanMake(0.05, 0.05)
-		let region = MKCoordinateRegionMake(location.coordinate, span)
-		self.mapView.setRegion(region, animated: true)
+		// Delete all annotations of Map view
+		for annotation in self.mapView.annotations {
+			self.mapView.removeAnnotation(annotation)
+		}
 	}
 	
 	@IBAction func closeModal(_ sender: Any) {
@@ -78,16 +75,204 @@ class PlayJourneyViewController: UIViewController {
 	}
 }
 
+// MARK: - Methods for handling Map view
+
+extension PlayJourneyViewController: MKMapViewDelegate {
+	
+	func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+		
+		guard !(annotation is MKUserLocation) else { return nil }
+		
+		let reuseId = "pin"
+		var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+		
+		if pinView == nil {
+			pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+		}
+		
+		pinView?.pinTintColor = UIColor.journeyMainColor
+		pinView?.canShowCallout = true
+		
+		return pinView
+	}
+	
+	public func animateLocationsOnMap(index: Int) {
+//		let location: MKPlacemark = self.schedules[index].location
+//		
+//		let annotation = MKPointAnnotation()
+//		annotation.coordinate = location.coordinate
+//		annotation.title = location.name
+//		
+//		if let city = location.locality, let state = location.administrativeArea, let country = location.country {
+//			annotation.subtitle = "\(city) \(state), \(country)"
+//		}
+//		
+//		print("LOCATION: \(location.name!)")
+//		
+//		self.mapView.addAnnotation(annotation)
+//		self.mapView.selectAnnotation(annotation, animated: true)
+//		
+//		let span = MKCoordinateSpanMake(2.0, 2.0)
+//		let region = MKCoordinateRegionMake(location.coordinate, span)
+//		self.mapView.setRegion(region, animated: true)
+		
+		var coordinates: [CLLocationCoordinate2D] = self.journey.getCoordinatesOfJourney()
+		
+		if (self.mapView.annotations.count == 0) && (self.mapView.overlays.count == 0) {
+			var annotations = [MKPointAnnotation]()
+			let schedules: [Schedule] = self.journey.schedules
+			
+			// Append all annotations of the schedules
+			for schedule in schedules {
+				let location: MKPlacemark = schedule.location
+				let annotation = MKPointAnnotation()
+				annotation.coordinate = location.coordinate
+				annotation.title = location.name
+				
+				if let city = location.locality, let state = location.administrativeArea, let country = location.country {
+					annotation.subtitle = "\(city) \(state), \(country)"
+				}
+				
+				annotations.append(annotation)
+				self.annotations = annotations
+			}
+			
+			self.mapView.addAnnotations(annotations)
+			
+			// Draw polyline on the map
+			let polyline = MKGeodesicPolyline(coordinates: coordinates, count: coordinates.count)
+			self.mapView.add(polyline)
+			self.polyline = polyline
+		}
+		
+		// Show annotations automatically on the map
+		self.mapView.selectAnnotation((self.annotations?[index])!, animated: true)
+		
+		// Zoom in selected location on the map
+		let span = MKCoordinateSpanMake(2.0, 2.0)
+		let region = MKCoordinateRegionMake(coordinates[index], span)
+		self.mapView.setRegion(region, animated: true)
+	}
+	
+	func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+		if overlay is MKPolyline {
+			let lenderer = MKPolylineRenderer(overlay: overlay);
+			lenderer.strokeColor = UIColor.journeyWarningColor.withAlphaComponent(0.7)
+			lenderer.lineWidth = 5.0
+			return lenderer
+		}
+		
+		return MKOverlayRenderer()
+	}
+}
+
 // MARK: - Implement methods of UICollectionViewDelegate and UICollectionViewDataSource
 
 extension PlayJourneyViewController: UICollectionViewDelegate, UICollectionViewDataSource {
 	
 	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		return 1
+		return self.allAssets.count
 	}
 	
 	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PlayImageCell", for: indexPath)
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PlayImageCell", for: indexPath) as! PlayImageCollectionViewCell
+		
+		let travelAsset: TravelAsset = self.allAssets[indexPath.row]
+		PHImageManager.default().requestImage(for: travelAsset.asset!, targetSize: cell.imageView.frame.size, contentMode: .aspectFill, options: nil, resultHandler: { (image, info) in
+			cell.imageView.image = image
+		})
+		
 		return cell
+	}
+}
+
+// MARK: - Methods for auto scrolling of UICollectionView
+
+extension PlayJourneyViewController {
+	
+	func configAutoScrollTimer() {
+		self.timer = Timer.scheduledTimer(timeInterval: 0.03, target: self, selector: #selector(autoScrollView), userInfo: nil, repeats: true)
+	}
+	
+	func deconfigAutoScrollTimer() {
+		self.timer?.invalidate()
+	}
+	
+	func onTimer() {
+		self.autoScrollView()
+	}
+	
+	func autoScrollView() {
+		let initialPoint = CGPoint(x: self.progress, y: 0.0)
+		
+		if __CGPointEqualToPoint(initialPoint, self.collectionView.contentOffset) {
+			let loIndex: Int = self.checkLocationIndex(self.progress / self.widthOfCell)
+			
+			if self.locationIndex != loIndex {
+				self.locationIndex = loIndex
+				self.animateLocationsOnMap(index: self.locationIndex)
+			}
+			
+			if (self.progress / self.widthOfCell) == self.scheduleOffset[self.locationIndex] {
+				self.animateLocationsOnMap(index: self.locationIndex)
+				self.locationIndex += 1
+				
+			}
+			
+			if self.progress < self.collectionView.contentSize.width {
+				self.progress += 5.0
+				self.contentWidth += 5.0
+			
+			} else {
+				deconfigAutoScrollTimer()
+				self.collectionView.contentOffset = CGPoint(x: 0.0, y: 0.0)
+				self.animateLocationsOnMap(index: 0)
+				return
+			}
+			
+			let offsetPoint: CGPoint = CGPoint(x: self.progress, y: 0.0)
+			self.collectionView.contentOffset = offsetPoint
+		
+		} else {
+			// If the user scrolls collection view, set contentOffset again
+			self.collectionView.contentOffset.x -= self.collectionView.contentOffset.x.truncatingRemainder(dividingBy: 5.0)
+			self.progress = self.collectionView.contentOffset.x
+		}
+	}
+	
+	func checkLocationIndex(_ location: CGFloat) -> Int {
+		for index in 0..<self.scheduleOffset.count {
+			if location < self.scheduleOffset[index] {
+				return index - 1
+			}
+		}
+		
+		return 0
+	}
+}
+
+// MARK: - Methods to prepare animation
+
+extension PlayJourneyViewController {
+	
+	func appendScheduleAssets() {
+		for schedule in self.journey.schedules {
+			var aScheduleAsset = [TravelAsset]()
+			let keys = schedule.assetsDict.keys
+			
+			for key in keys {
+				let travelAssets: [TravelAsset] = schedule.assetsDict[key]!
+				for asset in travelAssets {
+					aScheduleAsset.append(asset)
+					self.allAssets.append(asset)
+				}
+			}
+			
+			self.scheduleAssets.append(aScheduleAsset)
+			
+			let previousIndex: Int = self.scheduleOffset.count - 1
+			let offsetValue: CGFloat = CGFloat(aScheduleAsset.count) + self.scheduleOffset[previousIndex]
+			self.scheduleOffset.append(offsetValue)
+		}
 	}
 }
